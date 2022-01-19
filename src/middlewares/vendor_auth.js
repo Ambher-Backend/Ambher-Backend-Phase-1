2ad1aeebdc191d5e469b0852a23ff94efe5af1a2
venkat-jayaram <97839280+venkat-jayaram@users.noreply.github.com>
2022-01-19 19:27:29 +0530

    Create vendor_auth.js

---
+++ src/middlewares/vendor_auth.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+//internal imports
+const Vendor = require('../models/vendor');
+const commonUtils = require('../lib/common_utils');
+dotenv.config();
+
+//Vendor Authorisation middleware function
+const VendorAuth = async (req, res, next) => 
+{
+	try 
+    {
+		if (!req.body.currentToken)
+        {
+			throw new Error("Token not present");
+		}  
+		const token = req.body.currentToken;
+		const decoded = jwt.verify(token,process.env.JWT_KEY);
+		const vendor = await Vendor.findOne(
+        {
+			_id: decoded._id,
+			currentToken: token
+		});
+		if(!vendor) {
+			throw new Error ("Vendor is not authorised");
+		}
+		if(vendor.isVerified === false) {
+			throw new Error ("Vendor account not verified");
+		}
+		if(admin.isBlocked === true) {
+			throw new Error("Blocked for " + vendor.blockedReason);
+		}
+		req.user = vendor;
+		req.currentToken = token;
+		next();
+	} catch (err) {
+		commonUtils.errorLog(err.message);
+		res.send(commonUtils.responseUtil(401,null,err.message));
+	}
+};
+
+module.exports = VendorAuth;
