6a0575eb23c81b1c14d34511860dd71694a4e287
samarthya-jha <samarthya3011@gmail.com>
2022-01-28 22:40:31 +0530

    minor changes

---
+++ src/services/fetch_filtered_customers.js
@@ -0,0 +1,32 @@
+const Customer = require('../models/customer');
+
+const filter = async (filter) => {
+  const filteredCustomers = buildQueryAndExecute(filter);
+  return filteredCustomers;
+}
+
+const generateCompositeQuery = (filter) => {
+  let query = {};
+  if(filter['isVerified'] !== undefined) {
+    query['configuration.isVerified'] = filter['isVerified'];
+  }
+  if(filter['isBlocked'] !== undefined) {
+    query['configuration.isBlocked'] = filter['isBlocked'];
+  }
+  return query;
+}
+
+const buildQueryAndExecute = async (filter) => {
+  const compositeQuery = generateCompositeQuery(filter);
+  let filteredCustomers = []
+  if(filter['query'] !== undefined) {
+    filteredCustomers = await Customer.fuzzySearch(filter['query'], compositeQuery);
+  }
+  else {
+    filteredCustomers = await Customer.find(compositeQuery);
+  }
+  return filteredCustomers;
+}
+
+
+module.exports = {filter};
