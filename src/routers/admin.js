683edfa97c4dde230816193a9812d91b7d6cae67
samarthya-jha <samarthya3011@gmail.com>
2022-01-28 23:56:43 +0530

    Added vendor view and customer view details for admin

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -107,6 +107,7 @@ router.post('/verify-vendor', adminParamValidator.verifyVendorAccountValidation,
 })
 
 
+//view vendor list based on filters
 router.post('/vendors', adminParamValidator.listVendorsValidation, AdminAuth, async (req, res) => {
 	try{
 		const filteredVendors = await helper.listVendors(req.body);
@@ -118,6 +119,7 @@ router.post('/vendors', adminParamValidator.listVendorsValidation, AdminAuth, as
 })
 
 
+//view customer list based on filters
 router.post('/customers', adminParamValidator.listCustomersValidation, AdminAuth, async (req, res) => {
 	try {
 		const filteredCustomers = await helper.listCustomers(req.body);
@@ -128,5 +130,28 @@ router.post('/customers', adminParamValidator.listCustomersValidation, AdminAuth
 	}
 })
 
+
+//view individual vendor details
+router.get('/vendor-details/:vendorId', adminParamValidator.viewVendorDetailsValidation, AdminAuth, async (req, res) => {
+	try {
+		const vendorDetailsResponse = await helper.vendorDetails(req.params.vendorId);
+		res.send(commonUtils.responseUtil(200, vendorDetailsResponse, "Vendor Details"));
+	} catch (err) {
+		commonUtils.errorLog(err.message);
+		res.send(commonUtils.responseUtil(400, null, err.message));
+	}
+})
+
+
+//view individual customer details
+router.get('/customer-details/:customerId', adminParamValidator.viewCustomerDetailsValidation, AdminAuth, async (req, res) => {
+	try {
+		const customerDetailsResponse = await helper.customerDetails(req.params.customerId);
+		res.send(commonUtils.responseUtil(200, customerDetailsResponse, "Customer Details"));
+	} catch (err) {
+		commonUtils.errorLog(err.message);
+		res.send(commonUtils.responseUtil(400, null, err.message));
+	}
+})
 
 module.exports = router;
