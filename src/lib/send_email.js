642757b09bdec277e72ea296f882f9ac1df17e83
Arihant <dugarrarihantt2015@gmail.com>
2022-02-12 00:08:48 +0530

    Linting

--- src/lib/send_email.js
+++ src/lib/send_email.js
@@ -3,45 +3,56 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+
+const sendTestEmail = (receiverEmail, mailSubject, mailBody) => {
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: receiverEmail, // Change to your recipient
+    from: process.env.SENDER_EMAIL, // Change to your verified sender
+    subject: mailSubject,
+    text: mailBody,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log(`Email sent to ${receiverEmail}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    }); 
+};
+
+
+const sendProductionEmail = (receiverEmail, mailSubject, mailBody) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.SENDER_EMAIL,
+      pass: process.env.SENDER_EMAIL_PASSWORD,
+    },
+  });
+  console.log(`Email initiated for ${receiverEmail}`);
+  transporter.sendMail({
+    from: "Ambher Technologies",
+    to: receiverEmail,
+    subject: mailSubject,
+    text: mailBody,
+  });
+};
+
+
 // making it a non promising function to avoid blocking the main thread.
 const sendEmail = (receiverEmail, mailSubject, mailBody) => {
   switch (process.env.NODE_ENV) {
   case "production": {
-    const sgMail = require("@sendgrid/mail");
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: receiverEmail, // Change to your recipient
-      from: process.env.SENDER_EMAIL, // Change to your verified sender
-      subject: mailSubject,
-      text: mailBody,
-    };
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log(`Email sent to ${receiverEmail}`);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    sendProductionEmail(receiverEmail, mailSubject, mailBody);
     break;
   }
   case "test": {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SENDER_EMAIL_PASSWORD,
-      },
-    });
-    console.log(`Email initiated for ${receiverEmail}`);
-    transporter.sendMail({
-      from: "Ambher Technologies",
-      to: receiverEmail,
-      subject: mailSubject,
-      text: mailBody,
-    });
+    sendTestEmail(receiverEmail, mailSubject, mailBody);
     break;
   }
   }
