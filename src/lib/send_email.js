c5e828534cf80f624c9e14a34e0cda9961bb87c5
Arihant <dugarrarihantt2015@gmail.com>
2022-02-11 22:37:28 +0530

    SwitchCase

--- src/lib/send_email.js
+++ src/lib/send_email.js
@@ -5,47 +5,44 @@ dotenv.config();
 
 // making it a non promising function to avoid blocking the main thread.
 const sendEmail = (receiverEmail, mailSubject, mailBody) => {
-  if (
-    ["production", "test"].find((env) => env === process.env.NODE_ENV) !==
-    undefined
-  ) {
-    return;
-  }
-  if (process.env.NODE_ENV === "production") {
-    const sgMail = require("@sendgrid/mail");
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: receiverEmail, // Change to your recipient
-      from: process.env.SENDER_EMAIL, // Change to your verified sender
-      subject: mailSubject,
-      text: mailBody,
-    };
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log(`Email sent to ${receiverEmail}`);
-      })
-      .catch((error) => {
-        console.error(error);
+  switch (process.env.NODE_ENV) {
+    case "production":
+      const sgMail = require("@sendgrid/mail");
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: receiverEmail, // Change to your recipient
+        from: process.env.SENDER_EMAIL, // Change to your verified sender
+        subject: mailSubject,
+        text: mailBody,
+      };
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log(`Email sent to ${receiverEmail}`);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      break;
+    case "test":
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.SENDER_EMAIL,
+          pass: process.env.SENDER_EMAIL_PASSWORD,
+        },
       });
-  } else {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SENDER_EMAIL_PASSWORD,
-      },
-    });
 
-    console.log(`Email initiated for ${receiverEmail}`);
-    transporter.sendMail({
-      from: "Ambher Technologies",
-      to: receiverEmail,
-      subject: mailSubject,
-      text: mailBody,
-    });
+      console.log(`Email initiated for ${receiverEmail}`);
+      transporter.sendMail({
+        from: "Ambher Technologies",
+        to: receiverEmail,
+        subject: mailSubject,
+        text: mailBody,
+      });
+      break;
   }
 };
 
